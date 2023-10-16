2a51206873a6aa87de758e1e9842eddff5b7727b
marcelldac <dacmarcell@gmail.com>
2023-10-16 04:18:13 -0300

    feat: add create and read user methods

--- simple-server.ts
+++ simple-server.ts
@@ -1,17 +1,46 @@
 import { ApolloServer, gql } from "apollo-server";
+import {randomUUID} from "node:crypto";
 
 const typeDefs = gql`
+    type User{
+        id: String!
+        name: String!
+    }
+
     type Query {
-        helloWorld: String!
+        users: [User!]!
+    }
+
+    type Mutation {
+        createUser(name: String): User!
     }
 `
+
+interface User{
+    id: string;
+    name: string;
+}
+
+const users: User[] = [];
 
 const server = new ApolloServer({
     typeDefs,
     resolvers: {
         Query: {
-            helloWorld: () => {
-                return 'Hello World!';
+            users: () => {
+                return users;
+            }
+        },
+
+        Mutation: {
+            createUser: (_, args) => {
+                const user = {
+                    id: randomUUID(),
+                    name: args.name
+                }
+
+                users.push(user);
+                return user;
             }
         }
     }
